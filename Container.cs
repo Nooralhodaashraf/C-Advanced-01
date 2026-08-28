541f03e978f2790c4aedd0808d978430b205408b
Nooralhodaashraf <nooralhodaashraf@gmail.com>
2026-08-28 19:07:23 +0300

    Q2

---
+++ Container.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace C__Advanced_01
+{
+    internal class Container<T>
+    {
+        private T value;
+        public void Add(T item)
+        {
+            value = item;
+        }
+
+        public T Get()
+        {
+            return value;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -7,6 +7,20 @@
             #region Q1
             //What is a generic class? Why use generics?
             //generic class is a class that can work with different data types without specifying the exact type when the class is created using <T>=> for identfing any type 
+            //why generics :
+            // Type Safety - Code Reusability - Better Performance - Cleaner Code
+            #endregion
+
+            #region Q2
+            //Write a generic class Container<T> with Add and Get methods.
+            //class created ... lets use it :
+            //Container<int> intContainer = new Container<int>();
+            //intContainer.Add(100);
+            //Console.WriteLine(intContainer.Get());//100
+            ////================================================
+            //Container<string> stringContainer = new Container<string>();
+            //stringContainer.Add("Hello");
+            //Console.WriteLine(stringContainer.Get());//Hello
             #endregion
         }
     }
