312f65af09aa3a13bddeb7c76abfa2a3e9f73e9f
Nooralhodaashraf <nooralhodaashraf@gmail.com>
2026-08-28 19:53:50 +0300

    Q5

--- Program.cs
+++ Program.cs
@@ -31,6 +31,50 @@
             //Console.WriteLine(student.Key);//1
             //Console.WriteLine(student.Value);//Noor
             #endregion
+
+            #region Q4
+            //What is a generic method? Write Swap<T> method.
+            //generic method is a method that can work with different data types without specifying the exact type when the method is defined.
+            //method was created ...lets use it :
+            //int x = 10;
+            //int y = 20;
+
+            //Swap(ref x, ref y);
+
+            //Console.WriteLine(x); // 20
+            //Console.WriteLine(y); // 10
+            ////==========================================
+            //string first = "Hello";
+            //string second = "World";
+
+            //Swap(ref first, ref second);
+
+            //Console.WriteLine(first);  // World
+            //Console.WriteLine(second); // Hello
+            #endregion
+
+            #region Q5
+            //Write a generic method FindMax < T > that finds maximum value
+            //Method created ...lets use it :
+            //int maxInt = FindMax(10, 20);
+            //Console.WriteLine(maxInt); // 20
+            ////========================================================
+            //string maxString = FindMax("Apple", "Banana");
+            //Console.WriteLine(maxString); // Banana
+            #endregion
+        }
+
+        //Genaric Method => Q4
+        public static void Swap<T>(ref T a, ref T b)
+        {
+            T temp = a;
+            a = b;
+            b = temp;
+        }
+
+        public static T FindMax<T>(T a, T b) where T : IComparable<T>
+        {
+            return a.CompareTo(b) > 0 ? a : b;
         }
     }
 }
